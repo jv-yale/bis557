b7a2022b0f26d099c5e0bce2e42069fb3b07321a
jv-yale <jacob.vanhouten@yale.edu>
2020-09-17 16:40:30 -0400

    Initial commit

--- R/penguinsi-doc.R
+++ R/penguinsi-doc.R
@@ -5,7 +5,7 @@
 #' @rdname penguinsi
 #' @description
 #' The penguins data set after imputing all the missing values using the {missForest} package.
-#' Note: Even imputed outcomes. Bad juju.
+#' Note: Even imputed outcomes.
 #' @references See the palmerpenguins::penguins documentation for information about the values.
 #' @keywords data
 NULL
