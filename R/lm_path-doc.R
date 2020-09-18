f7f9aa9f9ea6a638788fbb5b49d135fdc2108621
jv-yale <jacob.vanhouten@yale.edu>
2020-09-18 19:21:56 -0400

    Initial commit

---
+++ R/lm_path-doc.R
@@ -0,0 +1,10 @@
+#' Data from lm_path.csv
+#'
+#' @name lm_patho
+#' @docType data
+#' @rdname lm_patho
+#' @description
+#' The lm_path data set for completing Homework 1
+#' @references None provided at this time.
+#' @keywords data
+NULL
---
+++ data-raw/make_path.R
@@ -0,0 +1,3 @@
+lm_patho<-as_tibble(read.csv("data-raw/lm_patho.csv", header=T))
+
+use_data(lm_patho)
