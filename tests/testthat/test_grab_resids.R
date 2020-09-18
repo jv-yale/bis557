60dfc8ce3d3daf235dbf6f3c18301776851ff41b
jv-yale <jacob.vanhouten@yale.edu>
2020-09-18 13:43:40 -0400

    Initial commit

---
+++ tests/testthat/test_grab_resids.R
@@ -0,0 +1,7 @@
+context("Testing grab resids")
+library(palmerpenguins)
+data(penguins)
+fit<-lm(bill_length_mm~., data=penguins[,-8])
+resids<-grab_resids(fit)
+expect_equal(resids, fit$residuals)
+
