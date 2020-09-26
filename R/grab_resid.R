9ff9914c48492b5db079b39317ae11939b0278f6
jv-yale <jacob.vanhouten@yale.edu>
2020-09-26 15:31:25 -0400

    Committing for the first time in a long time.

--- R/grab_resid.R
+++ R/grab_resid.R
@@ -1,6 +1,6 @@
 #' @title Grab the residuals
 #' This is a simple function to grab the residuals of a linear model
-#' @param lm_obj the linear model object created by the lm() function
+#' @param lm_ob the linear model object created by the lm() function
 #' @examples
 #' library(palmerpenguins)
 #' fit<-lm(penguins$bill_length_mm~., data=penguins[,-8])
